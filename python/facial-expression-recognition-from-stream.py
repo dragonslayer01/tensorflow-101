d42ed28052fe86da9813fd8f3f1764786fbd99a5
Shaishav Jain <34786550+dragonslayer01@users.noreply.github.com>
2018-10-12 11:52:25 +0530

    Update facial-expression-recognition-from-stream.py

--- python/facial-expression-recognition-from-stream.py
+++ python/facial-expression-recognition-from-stream.py
@@ -22,7 +22,11 @@ while(True):
 	ret, img = cap.read()
 	#img = cv2.imread('C:/Users/IS96273/Desktop/hababam.jpg')
 
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	if ret is True:
+   		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	else:
+   		continue
 
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
