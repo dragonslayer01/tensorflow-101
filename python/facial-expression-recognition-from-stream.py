0e80d1bd239f3332d79eb61df89d9be56dd6cea1
Shaishav Jain <34786550+dragonslayer01@users.noreply.github.com>
2018-10-12 12:29:25 +0530

    Update facial-expression-recognition-from-stream.py

--- python/facial-expression-recognition-from-stream.py
+++ python/facial-expression-recognition-from-stream.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 from keras.preprocessing import image
-import matplotlib.pyplot as plt
+import matplotlib as plt
 
 #-----------------------------
 #opencv initialization
