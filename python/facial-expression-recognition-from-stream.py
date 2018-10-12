44515c7ee659138ddc5863c96a94f44141e79634
Shaishav Jain <34786550+dragonslayer01@users.noreply.github.com>
2018-10-12 12:26:11 +0530

    Update facial-expression-recognition-from-stream.py

--- python/facial-expression-recognition-from-stream.py
+++ python/facial-expression-recognition-from-stream.py
@@ -19,42 +19,41 @@ model.load_weights('tensorflow-101/python/facial_expression_model_weights.h5') #
 
 emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
 
-while(True):
 	#ret, img = cap.read()
-	img = cv2.imread('tensorflow-101/python/bradley_angry.jpg')
-	ret = True
+img = cv2.imread('tensorflow-101/python/bradley_angry.jpg')
+ret = True
 
 	#gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	if ret is True:
-   		gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	else:
-   		continue
+if ret is True:
+	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+else:
+	continue
 
-	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
 	#print(faces) #locations of detected faces
 
-	for (x,y,w,h) in faces:
-		cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #draw rectangle to main image
+for (x,y,w,h) in faces:
+	cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2) #draw rectangle to main image
 		
-		detected_face = img[int(y):int(y+h), int(x):int(x+w)] #crop detected face
-		detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY) #transform to gray scale
-		detected_face = cv2.resize(detected_face, (48, 48)) #resize to 48x48
+	detected_face = img[int(y):int(y+h), int(x):int(x+w)] #crop detected face
+	detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY) #transform to gray scale
+	detected_face = cv2.resize(detected_face, (48, 48)) #resize to 48x48
 		
-		img_pixels = image.img_to_array(detected_face)
-		img_pixels = np.expand_dims(img_pixels, axis = 0)
+	img_pixels = image.img_to_array(detected_face)
+	img_pixels = np.expand_dims(img_pixels, axis = 0)
 		
-		img_pixels /= 255 #pixels are in scale of [0, 255]. normalize all pixels in scale of [0, 1]
+	img_pixels /= 255 #pixels are in scale of [0, 255]. normalize all pixels in scale of [0, 1]
 		
-		predictions = model.predict(img_pixels) #store probabilities of 7 expressions
+	predictions = model.predict(img_pixels) #store probabilities of 7 expressions
 		
 		#find max indexed array 0: angry, 1:disgust, 2:fear, 3:happy, 4:sad, 5:surprise, 6:neutral
-		max_index = np.argmax(predictions[0])
+	max_index = np.argmax(predictions[0])
 		
-		emotion = emotions[max_index]
+	emotion = emotions[max_index]
 		
 		#write emotion text above rectangle
-		cv2.putText(img, emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
+	cv2.putText(img, emotion, (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2)
 		
 		#process on detected face end
 		#-------------------------
@@ -63,9 +62,9 @@ while(True):
 
 	#if cv2.waitKey(1) & 0xFF == ord('q'): #press q to quit
 	#	break
-	plt.imshow(img, cmap = 'gray', interpolation = 'bicubic') 
-	plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
-	plt.show()
+plt.imshow(img, cmap = 'gray', interpolation = 'bicubic') 
+plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+plt.show()
 
 #kill open cv things		
 #cap.release()
